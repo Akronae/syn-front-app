44043d857f89b8122f286acd6f87c4acb9826c22
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-12-15 22:16:49 +0100

    created text-input

--- src/text.tsx
+++ src/text.tsx
@@ -50,6 +50,6 @@ const TextWrapper = styled(Base)``
 
 const TextBase = styled.Text`
   color: ${(p) => p.theme.colors.text.primary};
-  font-size: ${(p) => p.theme.typography.size.regular};
+  font-size: ${(p) => p.theme.typography.size.md};
   font-family: ${(p) => boldnessToFont(getStyleBoldness(p.style), p.theme)};
 `
