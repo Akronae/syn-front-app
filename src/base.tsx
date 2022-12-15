4bc08b4469f3c8929cdf783c28a327dafdacb6f6
Alexandre Daubricourt <alexandre@daubricourt.com>
2022-12-15 22:21:54 +0100

    scroll

--- src/base.tsx
+++ src/base.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react'
+import { forwardRef, PropsWithChildren, useEffect, useRef } from 'react'
 import * as React from 'react-native'
 import Animated from 'react-native-reanimated'
 import styled from 'styled-components/native'
@@ -9,13 +9,13 @@ export interface BaseProps<TStyle extends Record<string, any> = React.ViewStyle>
   showIf?: boolean
 }
 
-export const Base = (props: BaseProps) => {
+export const Base = forwardRef<Animated.View, BaseProps>((props: BaseProps, ref) => {
   const { children, showIf, ...passed } = props
 
   if (!showIf && showIf !== null && showIf !== undefined) return null
 
-  return <BaseWrapper {...passed}>{children}</BaseWrapper>
-}
+  return <BaseWrapper {...passed} ref={ref}>{children}</BaseWrapper>
+})
 
 export function takeBaseOwnProps<T extends BaseProps>(props: T) {
   const { children, style, showIf, entering, ...rest } = props
