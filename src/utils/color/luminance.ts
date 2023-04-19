269cf3a391622fdabe477ec9a169389ef4152b62
Alexandre Daubricourt <alexandre@daubricourt.com>
2023-04-19 12:36:33 +0200

    finished diag screen/contrast handling

--- src/utils/color/luminance.ts
+++ src/utils/color/luminance.ts
@@ -2,18 +2,15 @@ import { hexToRgba } from './hex-to-rgba'
 
 export function luminance(color: string): number {
   const rgb = hexToRgba(color)
-  const RsRGB = rgb.r / 255
-  const GsRGB = rgb.g / 255
-  const BsRGB = rgb.b / 255
-  const R =
-    RsRGB <= 0.03928 ? RsRGB / 12.92 : Math.pow((RsRGB + 0.055) / 1.055, 2.4)
-  const G =
-    GsRGB <= 0.03928 ? GsRGB / 12.92 : Math.pow((GsRGB + 0.055) / 1.055, 2.4)
-  const B =
-    BsRGB <= 0.03928 ? BsRGB / 12.92 : Math.pow((BsRGB + 0.055) / 1.055, 2.4)
+  const R = rgb.r / 255
+  const G = rgb.g / 255
+  const B = rgb.b / 255
+
+  const linear = (n: number) =>
+    n <= 0.03928 ? n / 12.92 : Math.pow((n + 0.055) / 1.055, 2.4)
 
   // For the sRGB colorspace, the relative luminance of a color is defined as:
-  const L = 0.2126 * R + 0.7152 * G + 0.0722 * B
+  const L = 0.2126 * linear(R) + 0.7152 * linear(G) + 0.0722 * linear(B)
 
   return L
 }
