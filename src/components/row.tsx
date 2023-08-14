15bb59dfdaf87c5a44d9e8e5b97bb5fd4101745e
Alexandre Daubricourt <alexandre@daubricourt.com>
2023-08-14 14:34:22 +0200

    created row elem

---
+++ src/components/row.tsx
@@ -0,0 +1,24 @@
+import { Base, BaseProps } from '@proto-native/components/base'
+import { themed } from '@proto-native/utils/theme/themed'
+import * as React from 'react'
+import { FlexStyle } from 'react-native'
+
+export type RowProps = BaseProps & {
+  justifyContent?: FlexStyle['justifyContent']
+  alignItems?: FlexStyle['alignItems']
+  gap?: number
+}
+
+export function Row(props: RowProps) {
+  const { ...passed } = props
+
+  return <RowBase {...passed}></RowBase>
+}
+
+const RowBase = themed<RowProps>(Base, (p) => ({
+  display: `flex`,
+  flexDirection: `row`,
+  justifyContent: p.justifyContent,
+  alignItems: p.alignItems,
+  gap: p.gap,
+}))
