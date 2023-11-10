829c1cc944025ae6dc197e6452656f03a17957f4
Alexandre Daubricourt <alexandre@daubricourt.com>
2023-11-10 23:30:12 +0100

    substitued

--- src/utils/expo-router/use-search-params-typed.ts
+++ src/utils/expo-router/use-search-params-typed.ts
@@ -18,24 +18,33 @@ export function useSearchParamsTyped<
   const searchParams = useSearchParams()
   const result = {} as any
   for (const key in params) {
+    const val = searchParams[key]
     if (params[key] === `string`) {
-      if (typeof searchParams[key] !== `string`)
-        throw new Error(`Invalid type for '${key}': '${params[key]}'`)
-      result[key] = searchParams[key]?.toString()
+      if (typeof val !== `string`)
+        throw new Error(
+          `Invalid type for '${key}': '${params[key]}. Value: '${val}''`,
+        )
+      result[key] = val?.toString()
     } else if (params[key] === `number`) {
-      if (Number.isNaN(Number(searchParams[key])))
-        throw new Error(`Invalid type for '${key}': '${params[key]}'`)
-      result[key] = Number(searchParams[key])
+      if (Number.isNaN(Number(val)))
+        throw new Error(
+          `Invalid type for '${key}': '${params[key]}. Value: '${val}''`,
+        )
+      result[key] = Number(val)
     } else if (params[key] === `string[]`) {
-      if (!Array.isArray(searchParams[key]))
-        throw new Error(`Invalid type for '${key}': '${params[key]}'`)
-      result[key] = searchParams[key]
+      if (!Array.isArray(val))
+        throw new Error(
+          `Invalid type for '${key}': '${params[key]}. Value: '${val}''`,
+        )
+      result[key] = val
     } else if (params[key] === `number[]`) {
-      if (!Array.isArray(searchParams[key]))
-        throw new Error(`Invalid type for '${key}': '${params[key]}'`)
-      result[key] = (searchParams[key] as Array<string>).map(Number)
+      if (!Array.isArray(val))
+        throw new Error(
+          `Invalid type for '${key}': '${params[key]}. Value: '${val}''`,
+        )
+      result[key] = (val as Array<string>).map(Number)
     } else {
-      throw new Error(`Invalid type ${params[key]}`)
+      throw new Error(`Invalid type ${params[key]}. Value: '${val}'`)
     }
   }
   return result
