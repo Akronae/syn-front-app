1febb50220deefecaca97809766b5dc34b4fcf94
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-01-06 23:27:10 +0100

    fixed scrollview indices

--- src/components/scroll-view.tsx
+++ src/components/scroll-view.tsx
@@ -5,19 +5,20 @@ import {
 } from '@proto-native/components/base'
 import * as Native from 'react-native'
 
-export type ScrollViewProps = BaseProps & Native.ScrollViewProps
-
-export function ScrollView(props: ScrollViewProps) {
-  const { children, ...passed } = props
-  const baseProps = takeBaseOwnProps(passed)
-
-  return (
-    <ScrollViewBase {...baseProps.taken}>
-      <Native.ScrollView {...baseProps.rest}>
-        <Base>{children}</Base>
-      </Native.ScrollView>
-    </ScrollViewBase>
-  )
+export type ScrollViewProps = Native.ScrollViewProps & {
+  gap?: Native.ViewStyle['gap']
+  flex?: Native.ViewStyle['flex']
 }
 
-const ScrollViewBase = Base
+export function ScrollView(props: ScrollViewProps) {
+  const { children, gap, flex, ...passed } = props
+
+  return (
+    <Native.ScrollView
+      {...passed}
+      contentContainerStyle={{ display: 'flex', flex, gap }}
+    >
+      {children}
+    </Native.ScrollView>
+  )
+}
