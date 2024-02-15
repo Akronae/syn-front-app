aebe7dc433503633e1c735058282f89e3bfcdcf7
Alexandre Daubricourt <alexandre@daubricourt.com>
2024-02-15 23:15:58 +0100

    removed useless comps

--- src/packages/proto-native/src/utils/device/is-web.ts
+++ src/packages/proto-native/src/utils/device/is-web.ts
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native'
 
 export function isWeb() {
-  return Platform.OS === `web`
+  return Platform.OS === `web` && typeof window !== `undefined`
 }
--- src/packages/proto-native/src/utils/i18n/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './country-flags'
-export * from './country-names'
